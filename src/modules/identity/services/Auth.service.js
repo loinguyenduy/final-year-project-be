48f3ad8e131d8897a5d8f0fd0e9ef7e200c5537c
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2026-05-14 02:26:02 +0700

    feat: register for handyman

--- src/modules/identity/services/Auth.service.js
+++ src/modules/identity/services/Auth.service.js
@@ -51,12 +51,14 @@ const handleRegisterUser = async (rawUserData) => {
       };
     }
 
+    const userRole = rawUserData.role || "CUSTOMER";
+
     const newUser = await User.create(
       {
         email: rawUserData.email,
         phone_number: rawUserData.phone_number || null,
         full_name: rawUserData.full_name,
-        role: "CUSTOMER", 
+        role: userRole,
       },
       { transaction: trans },
     );
@@ -71,6 +73,16 @@ const handleRegisterUser = async (rawUserData) => {
       },
       { transaction: trans },
     );
+
+    if (userRole === "HANDYMAN") {
+        await HandymanProfile.create({
+            user_id: newUser.id, 
+            kyc_status: 'UNVERIFIED',
+            bayesian_score: 5.00,
+            total_jobs_completed: 0,
+            security_bond_status: 'UNPAID'
+        }, { transaction: trans });
+    }
 
     // create verification token and send email
     const randomToken = crypto.randomBytes(32).toString("hex");
